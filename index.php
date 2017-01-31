e9b0196b2fe5323f8eaecd5059dc42b7fedd3826
arthur <arthur.belhandouz@wobility.fr>
2017-01-31 19:17:39 +0100

    problème de portabilité de connection pdo

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
 
+  include("user.php");
 
-  try{
-      $bdd = new PDO('mysql:host=localhost;dbname=side_project;charset=utf8', 'tetar9', 'tetar9');
-    }catch (Exception $e){
-        die('Erreur : ' . $e->getMessage());
-  }
+  try {
+    $strConnection = 'mysql:host=localhost;dbname=side_project'; //Ligne 1
+    $arrExtraParam= array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"); //Ligne 2
+    $pdo = new PDO($strConnection, 'tetar9', 'tetar9', $arrExtraParam); //Ligne 3; Instancie la connexion
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//Ligne 4
+}
+catch(PDOException $e) {
+    $msg = 'ERREUR PDO dans ' . $e->getFile() . ' L.' . $e->getLine() . ' : ' . $e->getMessage();
+    die($msg);
+}
+$query = 'SELECT * FROM user';
+$arr = $pdo->query($query)->fetch();
+
   $tmp = "fuck les nains";
   echo json_encode($tmp);
-echo "titi";
-
+  echo "titi";
+  $tetar = new humain();
+  print_r($arr);
 ?>
---
+++ user.php
@@ -0,0 +1,21 @@
+<?php
+
+  class humain{
+
+    private $_nom;
+    private $_prenom;
+    private $_age;
+    private $_adresse;
+    private $_telephone;
+
+
+
+    function __construct()
+    {
+        $this->_nom = "toto";
+    }
+
+  }
+
+
+?>
